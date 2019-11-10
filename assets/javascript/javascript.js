c2a7a1ace46a18c0271fb191889421fae0ac6852
Tian Qin <qintian29@gmail.com>
2019-11-10 22:40:40 +0800

    landing page rotating bar size and button text changed, landing page overflow problem solved, news API updated to show latest top news in Australia, add background for news section

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -108,11 +108,6 @@ $(document).ready(function() {
     lon = position.coords.longitude;
     console.log(lat);
     console.log(lon);
-    // x.innerHTML =
-    //   "Latitude: " +
-    //   position.coords.latitude +
-    //   "<br>Longitude: " +
-    //   position.coords.longitude;
   }
 
   function initMap() {
@@ -126,13 +121,16 @@ $(document).ready(function() {
   }
   //google api end//
   //news api start//
+
+  console.log("where is my news");
   function getNews() {
     let queryUrlNews =
-      "https://newsapi.org/v2/everything?q=coding&apiKey=b76dbe4baac44acda36c8e6baa935fb4";
+      "https://newsapi.org/v2/top-headlines?country=au&apiKey=b76dbe4baac44acda36c8e6baa935fb4";
     $.ajax({ url: queryUrlNews, method: "GET" }).then(response => {
-      let newsUrl = response.articles.url;
+      let newsUrl = response.articles[10].url;
       $("#newsIframe").attr("src", newsUrl);
     });
+    console.log(response.articles);
   }
 
   getNews();
@@ -379,5 +377,5 @@ $(document).ready(function() {
 
     $(exerciseSect).hide();
   });
-  //section Piotr end//
 });
+//section Piotr end//
